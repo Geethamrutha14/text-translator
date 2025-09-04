45670dff51c2834e93a4ced107aebf467d7926e1
Geethamrutha14 <geethamruthapalla4@email.com>
2025-09-04 22:29:12 +0530

    completed my front-end part

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { useState } from 'react'
 
 export default function App() {
   const [textinput , setTextInput] = useState();
+  const [language , setLanguage] = useState();
 
   console.log(textinput);
+  console.log(language);
   return (
     <div className='h-screen w-screen bg-slate-300 flex flex-col gap-5 items-center justify-center'>
        <div>
@@ -18,6 +20,15 @@ export default function App() {
 
         <textarea className='bg-white h-40 w-3xl border outline-none rounded-lg pr-2 pl-5 p-3'
         onChange={ (e)=> setTextInput(e.target.value) }></textarea>
+
+        <div>
+          <label htmlFor="language">Choose the language: </label>
+          <select name="language" defaultValue="en" className='bg-white rounded-lg p-2' onChange={(e)=> setLanguage(e.target.value)}>
+            <option value="en">Select</option>
+            <option value="hi">Hindi</option>
+            <option value="bn">Bengali</option>
+          </select>
+        </div>
 
         <button className=' w-full rounded-lg border bg-slate-700 p-3 text-white uppercase'>
         Translate
