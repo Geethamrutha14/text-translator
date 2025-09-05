34842f6a4ed8e153ac0ff7fc3cde940916b1fe0e
Geethamrutha14 <geethamruthapalla4@email.com>
2025-09-05 07:45:32 +0530

    added the text translation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios';
 
 export default function App() {
-  const [textinput , setTextInput] = useState();
-  const [language , setLanguage] = useState();
+  const [textinput , setTextInput] = useState("");
+  const [language , setLanguage] = useState("");
+  const [result , setResult] = useState("");
+
+  const handleTextTranslation = async ()=>{
+    try{
+      const options = {
+      method: 'POST',
+      url: 'https://google-translator9.p.rapidapi.com/v2',
+        headers: {
+          'x-rapidapi-key': 'e60eeaf26emshc822754762349fdp1bf8e4jsn7f909f479caa',
+          'x-rapidapi-host': 'google-translator9.p.rapidapi.com',
+          'Content-Type': 'application/json'
+        },
+        data: {
+        q: `${textinput}`,
+        source: 'en',
+        target: `${language}`,
+        format: 'text'
+        }
+      };
+      const response = await axios.request(options);
+      console.log(response.data.data.translations[Number(0)].translatedText);
+      setResult(response.data.data.translations[Number(0)].translatedText)
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
 
   console.log(textinput);
   console.log(language);
@@ -19,7 +47,7 @@ export default function App() {
         onChange={ (e)=> setTextInput(e.target.value) }></textarea>
 
         <textarea className='bg-white h-40 w-3xl border outline-none rounded-lg pr-2 pl-5 p-3'
-        onChange={ (e)=> setTextInput(e.target.value) }></textarea>
+        onChange={ (e)=> setTextInput(e.target.value) } value={result} readOnly></textarea>
 
         <div>
           <label htmlFor="language">Choose the language: </label>
@@ -30,7 +58,8 @@ export default function App() {
           </select>
         </div>
 
-        <button className=' w-full rounded-lg border bg-slate-700 p-3 text-white uppercase'>
+        <button className=' w-full rounded-lg border bg-slate-700 p-3 text-white uppercase'
+        onClick={handleTextTranslation}>
         Translate
         </button>
 
@@ -40,5 +69,4 @@ export default function App() {
        
 
     </div>
-  )
-}
+  )}
